ab663fa09370f6f827cccd8ea1047e271bd2e7e8
ennouflialae-coder <ennouflialae@gmail.com>
2026-05-04 19:04:51 +0100

    Add files via upload

---
+++ Views/Livraison/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TMS_Project.Views.Livraison
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
